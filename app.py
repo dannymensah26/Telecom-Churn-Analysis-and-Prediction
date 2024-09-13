4acc0191cbfab1e505973bcce6c2f66202058f67
Daniel Mensah <dannymensah26@gmail.com>
2024-09-13 17:45:53 -0400

    final

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ st.title('Telecom Customer Churn Prediction')
 
 
 # Display the image with caption
-st.image("C:/Telecom Churn Analysis and Prediction/data.JPG", 
+st.image("images/data.JPG", 
          caption="Welcome to the Churn Prediction App", 
          use_column_width=True)
 
